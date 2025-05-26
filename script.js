00002b0ba12affc839c0360499ced923002e29e9
GammonHaroldNg <harold.ng@gammonconstruction.com>
2025-05-26 17:38:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -123,6 +123,8 @@ async function toggleAR() {
         console.error('AR error:', error);
         alert(`Error: ${error.message}`);
     }
+}
+
 function toggleGyro() {
     if (!isMobile) return;
 
